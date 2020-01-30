d58530ff5aeb699f950c6eb0847f58aeaad456a3
ArjobanSingh <arjobansingh1998@gmail.com>
2020-01-30 17:57:19 +0530

    part 2 of week 1 done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import './App.css';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-<div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu />
-      </div>
-  );
+
+class App extends React.Component{
+  
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes : DISHES,
+    };
+  }
+
+  render(){
+    return (
+      <div className="App">
+              <Navbar dark color="primary">
+                <div className="container">
+                  <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                </div>
+              </Navbar>
+              <Menu dishes={this.state.dishes} />
+            </div>
+        );
+  }
 }
+
 
 export default App;
